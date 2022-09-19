d6a4c7da74ce96d923679c31fb94b243fdd61768
nazar-yednak <93836409+nazar-yednak@users.noreply.github.com>
2022-09-19 18:44:19 +0000

    Deploying to gh-pages from @ nazar-yednak/goit-react-hw-08-phonebook@8e16e325d384334003420b137f3bcae0bc9d2ab7 🚀

---
+++ static/js/561.5bc12efe.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[561],{6561:function(e,t,n){n.r(t);n(2791);var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Welcome"})})}}}]);
+//# sourceMappingURL=561.5bc12efe.chunk.js.map
